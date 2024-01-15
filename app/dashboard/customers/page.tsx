9556c9734b8062c047fe6b5144733bb60ba89e80
Eloi0424 <beichu0424@qq.com>
2024-01-15 10:31:40 +0800

    part 4

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+interface PageProps {}
+
+const Page: FunctionComponent<PageProps> = () => {
+  return <p>Customers Page</p>;
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+interface PageProps {}
+
+const Page: FunctionComponent<PageProps> = () => {
+  return <p>Invoices Page</p>;
+};
+
+export default Page;
